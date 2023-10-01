ffe8d3b70429ef446ddf68179edf92143331b0f1
Je Yun <alwaysblue21@gmail.com>
2023-10-01 17:48:01 -0400

    installed mongoose and connected database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LegoModule } from './lego/lego.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [LegoModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".env",
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    LegoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
