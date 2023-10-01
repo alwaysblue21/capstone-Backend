144d80edfb650c492f9cdaeb9d0fbff1eff1cc2c
Je Yun <alwaysblue21@gmail.com>
2023-10-01 19:00:30 -0400

    lego controller get

--- src/lego/lego.controller.ts
+++ src/lego/lego.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { LegoService } from './lego.service';
+import { Lego } from './schemas/lego.schema';
 
 @Controller('lego')
-export class LegoController {}
+export class LegoController {
+    constructor(private legoService: LegoService) {}
+
+    @Get()
+    async getAllLegos(): Promise<Lego[]> {
+        return this.legoService.findAll();
+    }
+}
