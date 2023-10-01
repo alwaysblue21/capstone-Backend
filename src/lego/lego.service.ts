f7cef515f0b3eadc274ad4bead05b5b99d94e4c3
Je Yun <alwaysblue21@gmail.com>
2023-10-01 18:46:45 -0400

    lego service

--- src/lego/lego.service.ts
+++ src/lego/lego.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Lego } from './schemas/lego.schema';
+import * as mongoose from 'mongoose';
 
 @Injectable()
-export class LegoService {}
+export class LegoService {
+    constructor(
+        @InjectModel(Lego.name)
+        private legoModel: mongoose.Model<Lego>
+    ) {}
+
+    async findAll(): Promise<Lego[]> {
+        const legos = await this.legoModel.find();
+        return legos;
+    }
+}
