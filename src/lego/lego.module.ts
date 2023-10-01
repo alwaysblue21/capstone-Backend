ca4969b6845a11cac1dffd9dabba051deee58c0a
Je Yun <alwaysblue21@gmail.com>
2023-10-01 18:29:32 -0400

    imported lego schema into lego module

--- src/lego/lego.module.ts
+++ src/lego/lego.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LegoController } from './lego.controller';
 import { LegoService } from './lego.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { LegoSchema } from './schemas/lego.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: "Lego", schema: LegoSchema }])],
   controllers: [LegoController],
   providers: [LegoService]
 })
