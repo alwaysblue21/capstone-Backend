2c2bce058181f96550af03a0ee7e6da6d4f492de
Je Yun <alwaysblue21@gmail.com>
2023-10-09 20:34:29 -0400

    lego dto

--- src/lego/dto/create-lego.dto.ts
+++ src/lego/dto/create-lego.dto.ts
@@ -1,10 +1,29 @@
+import { IsNotEmpty, IsString } from "class-validator";
 
 
 export class CreateLegoDto {
+
+    @IsNotEmpty()
+    @IsString()
     readonly name: string;
+
+    @IsNotEmpty()
+    @IsString()
     readonly image: string;
+
+    @IsNotEmpty()
+    @IsString()
     readonly age: string;
+
+    @IsNotEmpty()
+    @IsString()
     readonly pieces: string;
+
+    @IsNotEmpty()
+    @IsString()
     readonly item_number: string;
+
+    @IsNotEmpty()
+    @IsString()
     readonly price: string;
 }
--- src/lego/dto/update-lego.dto.ts
+++ src/lego/dto/update-lego.dto.ts
@@ -1,10 +1,29 @@
+import { IsOptional, IsString } from "class-validator";
 
 
 export class UpdateLegoDto {
+
+    @IsOptional()
+    @IsString()
     readonly name: string;
+
+    @IsOptional()
+    @IsString()
     readonly image: string;
+
+    @IsOptional()
+    @IsString()
     readonly age: string;
+
+    @IsOptional()
+    @IsString()
     readonly pieces: string;
+
+    @IsOptional()
+    @IsString()
     readonly item_number: string;
+
+    @IsOptional()
+    @IsString()
     readonly price: string;
 }
