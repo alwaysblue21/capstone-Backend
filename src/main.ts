d5fddf21130880d1c24ec110e85caa767010ec06
Je Yun <alwaysblue21@gmail.com>
2023-10-12 16:52:28 -0400

    cred

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import * as cookieParser from "cookie-parser"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
