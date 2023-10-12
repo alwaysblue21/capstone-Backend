fac8e5cf4a8cc0691bd62816c1631958660ca0cf
Je Yun <alwaysblue21@gmail.com>
2023-10-11 22:57:39 -0400

    post

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,7 +12,7 @@ export class AuthController {
         return this.authService.signUp(signUpDto)
     }
 
-    @Get("/login")
+    @Post("/login")
     login(@Body() loginDto: LoginDto): Promise<{ token: string}> {
         return this.authService.login(loginDto)
     }
