fc9bac849d2bd0dc1f5550809454a61b8d06e5d0
Je Yun <alwaysblue21@gmail.com>
2023-10-12 15:30:37 -0400

    cookie'

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from "cookie-parser"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    credentials: true,
-    origin: 'http://localhost:3000'
-  });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
