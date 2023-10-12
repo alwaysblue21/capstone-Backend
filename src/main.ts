b2f7b02fde3c1f08f4f3e9bb41bdb1e1e9c98b49
Je Yun <alwaysblue21@gmail.com>
2023-10-11 23:09:39 -0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
